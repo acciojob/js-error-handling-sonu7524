cca74098c2425efeaa19e41f9cd1a7c821d9a729
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2023-05-09 20:41:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+class OutOfRangeError extends Error {
+  constructor(message = "Expression should only consist of integers and +-/* characters and not <arg>") {
+    super(message);
+    this.name = "OutOfRangeError";
+  }
+}
+
+class InvalidExprError extends Error {
+  constructor(message = "Expression should not have an invalid combination of expression") {
+    super(message);
+    this.name = "InvalidExprError";
+  }
+}
